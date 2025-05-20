050da67243b3a510f7242051467c480109c1e1fc
falconzero7 <rfrodriguesap@gmail.com>
2025-05-20 17:38:45 -0300

    Update dashboard_mvi.py
    ("XlsxWriter")

--- dashboard_mvi.py
+++ dashboard_mvi.py
@@ -197,7 +197,7 @@ if len(anos) > 1:
 # 📥 Exportação
 def to_excel(dfs: dict):
     output = BytesIO()
-    with pd.ExcelWriter(output, engine='xlsmwriter') as writer:
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
         for name, df in dfs.items():
             df.to_excel(writer, sheet_name=name[:31], index=False)
     output.seek(0)
