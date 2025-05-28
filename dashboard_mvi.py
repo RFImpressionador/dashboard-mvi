d1df7c36e7962253485bed254b016156fe04f223
falconzero7 <rfrodriguesap@gmail.com>
2025-05-28 19:23:36 -0300

    Update dashboard_mvi.py

--- dashboard_mvi.py
+++ dashboard_mvi.py
@@ -20,8 +20,7 @@ from analises import (
     mostrar_dias_sem_morte,
     mostrar_total_por_cidade,
     mostrar_comparativo_ano,
-    mostrar_comparativo_mes,
-    mostrar_tabela_detalhes
+    mostrar_comparativo_mes
 )
 import pandas as pd
 from datetime import datetime
@@ -42,7 +41,7 @@ if df.empty:
 with st.sidebar:
     st.markdown("""
         <div style='text-align: center;'>
-            <img src='logo_p2_10bpm.png' width='80' style='border-radius: 50%;'>
+            <img src='logo_p2_10bpm.png' width='80' style='border-radius: 50%; display:block; margin:auto;'>
         </div>
         <hr style='border-top: 1px solid #aaa;'>
         <h4 style='color:#dc3545;'>🔎 Filtros</h4>
@@ -51,13 +50,13 @@ with st.sidebar:
 
     st.markdown("""
         <hr style='border-top: 1px solid #aaa;'>
-        <h4 style='color:#dc3545;'>🧭 Navegação</h4>
+        <h4 style='color:#dc3545;'>🧽 Navegação</h4>
         <ul style='list-style: none; padding-left: 0;'>
             <li>⏳ <a href="#dias-sem-mortes-por-cidade" style="text-decoration: none; color: white;">Dias sem Mortes</a></li>
             <li>🔢 <a href="#total-por-cidade-e-categoria" style="text-decoration: none; color: white;">Total por Cidade</a></li>
             <li>📈 <a href="#comparativo-cvli-ano-a-ano" style="text-decoration: none; color: white;">Comparativo Ano</a></li>
             <li>📊 <a href="#comparativo-cvli-mes-a-mes" style="text-decoration: none; color: white;">Comparativo Mês</a></li>
-            <li>📅 <a href="#datas-e-dias-da-semana-por-cidade" style="text-decoration: none; color: white;">Datas Detalhadas</a></li>
+            <li>🗕️ <a href="#datas-e-dias-da-semana-por-cidade" style="text-decoration: none; color: white;">Datas Detalhadas</a></li>
         </ul>
         <hr style='border-top: 1px solid #aaa;'>
         <small style='color:gray;'>Criado por Analista de Campo — Codinome: <strong>Falcão</strong></small>
@@ -73,12 +72,10 @@ st.markdown("<div style='margin-top: -40px'></div>", unsafe_allow_html=True)
 mostrar_dias_sem_morte(df, cidades)
 mostrar_total_por_cidade(df_filtrado, cidades)
 
-# 📊 Exibição Condicional de Comparativos
 if len(anos) >= 2:
     mostrar_comparativo_ano(df_filtrado, cidades)
 elif len(anos) == 1:
     mostrar_comparativo_mes(df_filtrado, cidades, anos, meses)
-    mostrar_tabela_detalhes(df_filtrado, anos, meses)
 
-# 📥 Botão exportar
-st.download_button("📅 Baixar Tabelas em Excel", data=to_excel({"Dados Filtrados": df_filtrado}), file_name="dados_filtrados.xlsx")
+# 📅 Botão exportar
+st.download_button("🗓️ Baixar Tabelas em Excel", data=to_excel({"Dados Filtrados": df_filtrado}), file_name="dados_filtrados.xlsx")
