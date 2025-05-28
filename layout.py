1cf40db955dd1186c8fbab802e770bc0b682b6ea
falconzero7 <rfrodriguesap@gmail.com>
2025-05-28 18:05:25 -0300

    Update layout.py

--- layout.py
+++ layout.py
@@ -6,18 +6,18 @@ def montar_sidebar():
         st.image("logo_p2_10bpm.png", width=80)
         st.markdown("<hr style='border-top: 1px solid #aaa;'>", unsafe_allow_html=True)
 
-        # Filtros são renderizados por filtros.py
+        # Filtros serão renderizados por filtros.py
         st.markdown("<h4 style='color:#dc3545;'>🔎 Filtros</h4>", unsafe_allow_html=True)
 
         # Navegação
-        st.markdown("### 🧭 Navegação")
+        st.markdown("### 🧽 Navegação")
         st.markdown("""
             <ul style='list-style:none; padding-left:0;'>
                 <li>⏳ <span style='color:#ccc;'>Dias sem Mortes</span></li>
-                <li>🔢 <span style='color:#ccc;'>Total por Cidade</span></li>
+                <li>🕢 <span style='color:#ccc;'>Total por Cidade</span></li>
                 <li>📈 <span style='color:#ccc;'>Comparativo Ano</span></li>
                 <li>📊 <span style='color:#ccc;'>Comparativo Mês</span></li>
-                <li>📅 <span style='color:#ccc;'>Datas Detalhadas</span></li>
+                <li>🗕️ <span style='color:#ccc;'>Datas Detalhadas</span></li>
             </ul>
         """, unsafe_allow_html=True)
 
