026706ae6bd8f5c916283e7c550a067265977aaa
falconzero7 <rfrodriguesap@gmail.com>
2025-05-28 13:05:55 -0300

    Create exportacao.py
    exportacao

---
+++ exportacao.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from io import BytesIO
+
+def to_excel(dfs: dict):
+    output = BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        for name, df in dfs.items():
+            df.to_excel(writer, sheet_name=name[:31], index=False)
+    output.seek(0)
+    return output
